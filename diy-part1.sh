3f6535e5b2551a80f218ca885794a7f19799b8c6
FincerDW <davidyang9264@gmail.com>
2024-06-28 05:29:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git adguardhome https://github.com/kenzok8/small-package/tree/main/adguardhome' >>feeds.conf.default
 # echo 'src-git luciadguardhome https://github.com/kenzok8/small-package/tree/main/luci-app-adguardhome' >>feeds.conf.default
@@ -27,5 +27,5 @@ echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.defau
 # echo 'src-git singbox https://github.com/kenzok8/small-package/tree/main/sing-box' >>feeds.conf.default
 # echo 'src-git smartdns https://github.com/kenzok8/small-package/tree/main/smartdns' >>feeds.conf.default
 # echo 'src-git lucismartdns https://github.com/kenzok8/small-package/tree/main/luci-app-smartdns' >>feeds.conf.default
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default 
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default 
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default 
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default 
